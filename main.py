73b9cb920693498237e86b5df997cbd484634ddf
eringiglio <eringiglio@gmail.com>
2016-08-08 10:39:31 -0500

    Updating code pulling habitica tasks to compare them with todoist tasks

--- main.py
+++ main.py
@@ -5,67 +5,207 @@ Phil Adams http://philadams.net
 habitica: commandline interface for http://habitica.com
 http://github.com/philadams/habitica
 
+
 Changes I've made essentially revolve around tweaking a command-line utility running in python
 to be a module holding a series of smaller python utilities that can be called within a larger program. 
 
 
-
-habitica: commandline interface for http://habitica.com
-http://github.com/philadams/habitica
-
-Usage: habitica [--version] [--help]
-                <command> [<args>...] [--difficulty=<d>]
-                [--verbose | --debug]
-
-    Options:
-      -h --help         Show this screen
-      --version         Show version
-      --difficulty=<d>  (easy | medium | hard) [default: easy]
-      --verbose         Show some logging information
-      --debug           Some all logging information
-
-    The habitica commands are:
-      status                 Show HP, XP, GP, and more
-      habits                 List habit tasks
-      habits up <task-id>    Up (+) habit <task-id>
-      habits down <task-id>  Down (-) habit <task-id>
-      dailies                List daily tasks
-      dailies done           Mark daily <task-id> complete
-      dailies undo           Mark daily <task-id> incomplete
-      todos                  List todo tasks
-      todos done <task-id>   Mark one or more todo <task-id> completed
-      todos add <task>       Add todo with description <task>
-      server                 Show status of Habitica service
-      home                   Open tasks page in default browser
-
-    For `habits up|down`, `dailies done|undo`, and `todos done`, you can pass
-    one or more <task-id> parameters, using either comma-separated lists or
-    ranges or both. For example, `todos done 1,3,6-9,11`.
-    """
-
-def setup():
+"""
 	
-    # set up args
-    args = docopt(cli.__doc__, version=VERSION)
+from bisect import bisect
+import json
+import logging
+import netrc
+import os.path
+from time import sleep
+from webbrowser import open_new_tab
 
-    # set up logging
-    if args['--verbose']:
-        logging.basicConfig(level=logging.INFO)
-    if args['--debug']:
-        logging.basicConfig(level=logging.DEBUG)
+from docopt import docopt
 
-    logging.debug('Command line args: {%s}' %
-                  ', '.join("'%s': '%s'" % (k, v) for k, v in args.items()))
+from habitica import api
 
-    # Set up auth
-    auth = load_auth(AUTH_CONF)
+from pprint import pprint
 
-    # Prepare cache
-    cache = load_cache(CACHE_CONF)
+try:
+    import ConfigParser as configparser
+except:
+    import configparser	
+	
+VERSION = 'habitica version 0.0.12'
+TASK_VALUE_BASE = 0.9747  # http://habitica.wikia.com/wiki/Task_Value
+HABITICA_REQUEST_WAIT_TIME = 0.5  # time to pause between concurrent requests
+HABITICA_TASKS_PAGE = '/#/tasks'
+# https://trello.com/c/4C8w1z5h/17-task-difficulty-settings-v2-priority-multiplier
+PRIORITY = {'easy': 1,
+            'medium': 1.5,
+            'hard': 2}
+AUTH_CONF = os.path.expanduser('~') + '/.config/habitica/auth.cfg'
+CACHE_CONF = os.path.expanduser('~') + '/.config/habitica/cache.cfg'
 
-    # instantiate api service
-    hbt = api.Habitica(auth=auth)
+SECTION_CACHE_QUEST = 'Quest'
 
+def load_auth(configfile):
+    """Get authentication data from the AUTH_CONF file."""
+
+    logging.debug('Loading habitica auth data from %s' % configfile)
+
+    try:
+        cf = open(configfile)
+    except IOError:
+        logging.error("Unable to find '%s'." % configfile)
+        exit(1)
+
+    config = configparser.SafeConfigParser()
+    config.readfp(cf)
+
+    cf.close()
+
+    # Get data from config
+    rv = {}
+    try:
+        rv = {'url': config.get('Habitica', 'url'),
+              'x-api-user': config.get('Habitica', 'login'),
+              'x-api-key': config.get('Habitica', 'password')}
+
+    except configparser.NoSectionError:
+        logging.error("No 'Habitica' section in '%s'" % configfile)
+        exit(1)
+
+    except configparser.NoOptionError as e:
+        logging.error("Missing option in auth file '%s': %s"
+                      % (configfile, e.message))
+        exit(1)
+
+    # Return auth data as a dictionnary
+    return rv
+
+
+def load_cache(configfile):
+    logging.debug('Loading cached config data (%s)...' % configfile)
+
+    defaults = {'quest_key': '',
+                'quest_s': 'Not currently on a quest'}
+
+    cache = configparser.SafeConfigParser(defaults)
+    cache.read(configfile)
+
+    if not cache.has_section(SECTION_CACHE_QUEST):
+        cache.add_section(SECTION_CACHE_QUEST)
+
+    return cache
+
+
+def update_quest_cache(configfile, **kwargs):
+    logging.debug('Updating (and caching) config data (%s)...' % configfile)
+
+    cache = load_cache(configfile)
+
+    for key, val in kwargs.items():
+        cache.set(SECTION_CACHE_QUEST, key, val)
+
+    with open(configfile, 'wb') as f:
+        cache.write(f)
+
+    cache.read(configfile)
+
+    return cache
+
+
+def get_task_ids(tids):
+    """
+    handle task-id formats such as:
+        habitica todos done 3
+        habitica todos done 1,2,3
+        habitica todos done 2 3
+        habitica todos done 1-3,4 8
+    tids is a seq like (last example above) ('1-3,4' '8')
+    """
+    logging.debug('raw task ids: %s' % tids)
+    task_ids = []
+    for raw_arg in tids:
+        for bit in raw_arg.split(','):
+            if '-' in bit:
+                start, stop = [int(e) for e in bit.split('-')]
+                task_ids.extend(range(start, stop + 1))
+            else:
+                task_ids.append(int(bit))
+    return [e - 1 for e in set(task_ids)]
+
+
+def updated_task_list(tasks, tids):
+    for tid in sorted(tids, reverse=True):
+        del(tasks[tid])
+    return tasks
+
+
+def print_task_list(tasks):
+    for i, task in enumerate(tasks):
+        completed = 'x' if task['completed'] else ' '
+        print('[%s] %s %s' % (completed, i + 1, task['text'].encode('utf8')))
+
+
+def qualitative_task_score_from_value(value):
+    # task value/score info: http://habitica.wikia.com/wiki/Task_Value
+    scores = ['*', '**', '***', '****', '*****', '******', '*******']
+    breakpoints = [-20, -10, -1, 1, 5, 10]
+    return scores[bisect(breakpoints, value)]
+
+def setup():		
+	"""
+	Adapting cli...
+	Usage: habitica [--version] [--help]
+			<command> [<args>...] [--difficulty=<d>]
+			[--verbose | --debug]
+
+	Options:
+	-h --help         Show this screen
+	--version         Show version
+	--difficulty=<d>  (easy | medium | hard) [default: easy]
+	--verbose         Show some logging information
+	--debug           Some all logging information
+
+	The habitica commands are:
+	status                 Show HP, XP, GP, and more
+	habits                 List habit tasks
+	habits up <task-id>    Up (+) habit <task-id>
+	habits down <task-id>  Down (-) habit <task-id>
+	dailies                List daily tasks
+	dailies done           Mark daily <task-id> complete
+	dailies undo           Mark daily <task-id> incomplete
+	todos                  List todo tasks
+	todos done <task-id>   Mark one or more todo <task-id> completed
+	todos add <task>       Add todo with description <task>
+	server                 Show status of Habitica service
+	home                   Open tasks page in default browser
+
+	For `habits up|down`, `dailies done|undo`, and `todos done`, you can pass
+	one or more <task-id> parameters, using either comma-separated lists or
+	ranges or both. For example, `todos done 1,3,6-9,11`.
+	"""
+	# set up args
+	args = docopt(cli.__doc__, version=VERSION)
+
+	# set up logging
+#	def verbose():
+#		logging.basicConfig(level=logging.INFO)
+#		return
+#	def debug():
+#		logging.basicConfig(level=logging.DEBUG)
+#		return
+
+	logging.debug('Command line args: {%s}' %
+				  ', '.join("'%s': '%s'" % (k, v) for k, v in args.items()))
+
+	# Set up auth
+	auth = load_auth(AUTH_CONF)
+
+	# Prepare cache
+	cache = load_cache(CACHE_CONF)
+
+	# instantiate api service
+	hbt = api.Habitica(auth=auth)
+	return
+	
     # GET server status
 def server():
     server = hbt.status()
