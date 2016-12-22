992df11cf72998e8c0ee151acd39f7a9cefad8cc
eringiglio <eringiglio@gmail.com>
2016-12-22 09:26:37 -0600

    Removing original lines of code now ported to main

--- basicSync.py
+++ basicSync.py
@@ -37,24 +37,8 @@ tod_inboxID = tod_projects[0].data['id']
 #Telling the site where the config stuff for Habitica can go and get a list of habitica tasks...
 auth, hbt = main.get_started('auth.cfg')  
 
-#Todoist tasks are, I think, classes. Let's make Habitica tasks classes, too.
-url = 'https://habitica.com/api/v3/tasks/user/'
-response = requests.get(url,headers=auth)
-hab_raw = response.json()
-hab_tasklist = hab_raw['data'] #FINALLY getting something I can work with... this will be a list of dicts I want to turn into a list of objects with class hab_tasks. Hrm. Weeeelll, if I make a class elsewhere....
-
-#keeping records of all our tasks
-hab_tasks = [] 
-
-#No habits right now, I'm afraid, in hab_tasks--Todoist gets upset. So we're going to make a list of dailies and todos instead...
-for task in hab_tasklist: 
-    item = HabTask(task)
-    if item.category == 'reward':
-        pass
-    elif item.category == 'habit': 
-        pass
-    else:
-        hab_tasks.append(item)
+#Getting all complete and incomplete habitica dailies and todos
+hab_tasks, r1, r2 = main.get_all_habtasks(auth)
 
 #Okay, now I need a list of todoist tasks. How do achieve that. 
 tod_tasks = []
