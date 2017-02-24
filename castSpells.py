53582a9c859d95bd63ad363744318a206ae0fd69
eringiglio <eringiglio@gmail.com>
2017-02-24 16:15:33 -0600

    Removing all trace of Habitica API from code

--- castSpells.py
+++ castSpells.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from habitica import api 
 import requests
 import json
 from hab_task import HabTask
@@ -7,6 +6,6 @@ import os
 import main
 import manaPull
 
-auth, hbt = main.get_started('auth.cfg')
+auth = main.get_started('auth.cfg')
 
 manaPull.cast_all_mana(auth,'valorousPresence')
