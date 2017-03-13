d86a53d55f6cfc1faaaaabc0b7a758b012123521
eringiglio <eringiglio@gmail.com>
2017-03-12 23:47:46 -0500

    tweaking setup for pip package

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 
-"""
-Here are things you'll need to install before it runs...
-"""
+from setuptools import setup
 
-pip install pytodoist
-pip install scriptabit
-"""
-We can't directly install the main habitica pip because the API is broken. Until philadams 
-gets around to to accepting the pull request, we'll need to install a workaround from synacktic...
-"""
-git clone https://github.com/synacktic/habitica
-pip install -e habitica
+setup(
+    name='habitica-todo',
+    version='1.2',
+    scripts=['oneWaySync'])
